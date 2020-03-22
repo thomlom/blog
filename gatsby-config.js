5741260415aa4f61f98b31b88fe1c6a96647bea2
Thomas Lombart <t.lombart97@gmail.com>
2020-03-22 12:50:13 +0100

    Remove google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -48,12 +48,6 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: `UA-161170715-1`,
-      },
-    },
     // `gatsby-plugin-feed`,
     {
       resolve: `gatsby-plugin-manifest`,
