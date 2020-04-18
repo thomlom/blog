c588b045295b10b4497ea38da0112a2cf088bc63
thomlom <t.lombart97@gmail.com>
2020-04-18 14:29:09 +0200

    Modify newsletter description

--- src/components/newsletter.js
+++ src/components/newsletter.js
@@ -10,19 +10,17 @@ const Newsletter = ({ inPost }) => (
     }
     className="gradient flex w-full flex flex-col shadow-lg rounded mt-6 p-2 px-4 md:px-24"
   >
-    <p className="mt-2 mb-1 leading-tight text-2xl text-gray-100 dark:text-gray-100 font-extrabold">
+    <p className="mt-2 mb-1 leading-tight text-2xl text-white font-extrabold">
       {inPost
         ? "Pssst... You can join the newsletter and read an exclusive post!"
         : "Join the newsletter and read an exclusive post!"}
     </p>
-    <p
-      className="mt-0 mb-1 text-gray-100 dark:text-gray-200 font-semibold"
-      v-if="inPost"
-    >
-      Learn how to get a job as a front-end developer, and get my latest posts
-      directly in your inbox{" "}
-      <span role="img" aria-label="Face with heart eyes">
-        😍
+    <p className="my-1 text-gray-100 font-semibold" v-if="inPost">
+      Get updates when I release new content and more. The next posts will focus
+      on advanced JavaScript, tooling and design. Ready to take your front-end
+      skills to the next level?{" "}
+      <span role="img" aria-label="Roeckt">
+        🚀
       </span>
     </p>
     <label htmlFor="bd-email" className="flex flex-col mt-2">
@@ -39,7 +37,7 @@ const Newsletter = ({ inPost }) => (
     <input
       type="submit"
       value="Subscribe"
-      className="bg-gray-200 dark:bg-gray-200 text-gray-900 dark:text-gray-900 rounded shadow w-full mt-3 font-bold px-3 py-2 cursor-pointer"
+      className="bg-gray-200 text-gray-900 rounded w-full mt-3 font-bold px-3 py-2 cursor-pointer shadow dark:bg-gray-200 dark:text-gray-900 "
     />
     <div className="flex flex-col sm:flex-row justify-between mt-2">
       <p className="text-gray-300 dark:text-gray-300 text-xs my-1">
