61fc6b6e63e4043e5023afbaaf80318b3c090b7f
thomlom <t.lombart97@gmail.com>
2020-04-19 14:47:58 +0200

    Modify newsletter wording

--- src/components/newsletter.js
+++ src/components/newsletter.js
@@ -8,7 +8,7 @@ const Newsletter = ({ inPost }) => (
     onSubmit={() =>
       window.open("https://buttondown.email/thomlom", "popupwindow")
     }
-    className="gradient flex w-full flex flex-col shadow-lg rounded mt-5 p-2 px-4 md:px-24"
+    className="gradient flex w-full flex flex-col shadow-lg rounded mt-5 p-2 px-4 md:px-20"
   >
     <p className="mt-2 mb-1 leading-tight text-xl sm:text-2xl text-white font-extrabold">
       {inPost
@@ -16,9 +16,9 @@ const Newsletter = ({ inPost }) => (
         : "Join the newsletter and read an exclusive post!"}
     </p>
     <p className="my-1 text-gray-100 font-semibold">
-      You'll also get updates when I release new content and more. The next
-      posts will focus on advanced JavaScript, tooling and design. Ready to take
-      your front-end skills to the next level?{" "}
+      You'll also get updates when I release new content and more. The upcoming
+      posts focus on advanced JavaScript, tooling and design. Ready to take your
+      front-end skills to the next level?{" "}
       <span role="img" aria-label="Roeckt">
         🚀
       </span>
