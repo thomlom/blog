0c7f05d9ba3c523c46e42c25e9687fc85864f079
thomlom <t.lombart97@gmail.com>
2020-04-24 17:42:37 +0200

    Add personal section homepage and revamp about me

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,11 @@
 import React from "react"
 import { graphql } from "gatsby"
+import Img from "gatsby-image"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const NotFoundPage = ({ data }) => {
-  const siteTitle = data.site.siteMetadata.title
-
+const NotFoundPage = ({ data: { photo } }) => {
   const textClassNames =
     "text-lg sm:text-2xl text-gray-600 dark:text-gray-500 mt-2 font-semibold leading-snug mt-4"
 
@@ -14,18 +13,17 @@ const NotFoundPage = ({ data }) => {
     "underline text-gray-800 hover:text-gray-900 dark:text-gray-300 dark:hover:text-gray-200"
 
   return (
-    <Layout title={siteTitle}>
+    <Layout>
       <SEO title="About me" />
-      <p className="text-xl sm:text-3xl text-gray-800 dark:text-gray-200 font-extrabold leading-tight">
-        Hey there!{" "}
-        <span role="img" aria-label="Waving hand">
-          👋
-        </span>
-        <br />
-        I'm Thomas Lombart, a front-end engineer.
-      </p>
+      <Img
+        fluid={photo.childImageSharp.fluid}
+        className="rounded-lg shadow-xl"
+      />
+      <h2 className="text-xl sm:text-3xl text-gray-800 dark:text-gray-200 font-extrabold leading-tight mt-6">
+        Hello, I'm Thomas Lombart.
+      </h2>
       <p className={textClassNames}>
-        I currently work for{" "}
+        I'm a french front-end engineer. I currently work for{" "}
         <a
           href="https://www.backmarket.com"
           className={linkClassNames}
@@ -35,12 +33,14 @@ const NotFoundPage = ({ data }) => {
           Back Market
         </a>
         .
-        <br />I write about front-end development whether it's JavaScript stuff,
-        performance, accessibility or design.
+      </p>
+      <p className={textClassNames}>
+        I help developers level-up their front-end skills through high-quality
+        posts on JavaScript, modern frameworks, design, and more.
       </p>
       <p className={textClassNames}>
         I love productivity and minimalism. I'm the guy who focuses on what
-        matters. I value privacy and ecology.
+        matters. I care about ecology and privacy.
       </p>
       <p className={textClassNames}>
         On my spare time, I train, travel, hike, watch good movies and listen to
@@ -51,37 +51,7 @@ const NotFoundPage = ({ data }) => {
         <a className={linkClassNames} href="mailto:t.lombart97@gmail.com">
           by mail
         </a>
-        . I usually answer within one business day 💌
-      </p>
-      <p className={textClassNames}>
-        Find me on{" "}
-        <a
-          href="https://twitter.com/thomas_lombart"
-          className={linkClassNames}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Twitter
-        </a>
-        ,{" "}
-        <a
-          className={linkClassNames}
-          href="https://github.com/thomlom"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          GitHub
-        </a>{" "}
-        or{" "}
-        <a
-          href="https://fr.linkedin.com/in/thomas-lombart"
-          className={linkClassNames}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          LinkedIn
-        </a>
-        .
+        . I usually answer within one business day.
       </p>
     </Layout>
   )
@@ -91,9 +61,12 @@ export default NotFoundPage
 
 export const pageQuery = graphql`
   query {
-    site {
-      siteMetadata {
-        title
+    photo: file(absolutePath: { regex: "/nature.jpg/" }) {
+      publicURL
+      childImageSharp {
+        fluid(maxWidth: 1000, maxHeight: 500) {
+          ...GatsbyImageSharpFluid
+        }
       }
     }
   }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { graphql } from "gatsby"
 import Img from "gatsby-image"
+import { useMedia } from "react-use"
 
 import Layout from "../components/layout"
 import Newsletter from "../components/newsletter"
@@ -8,33 +9,83 @@ import PostInfos from "../components/postInfos"
 import SEO from "../components/seo"
 import TransitionLink from "../components/transitionLink"
 
+import Twitter from "../icons/twitter.png"
+import GitHubLight from "../icons/github_light.png"
+import GitHubDark from "../icons/github_dark.png"
+
 const marginBetweenSections = "mt-6 sm:mt-10"
 
-const PostSection = ({ title, posts }) => {
+const PostSection = ({ title, posts }) => (
+  <div className={marginBetweenSections}>
+    <h4 className="text-lg sm:text-xl text-gray-900 dark:text-gray-100 uppercase tracking-wide font-extrabold">
+      {title}
+    </h4>
+    <div className="sm:mt-1 h-1 gradient-right rounded w-full -p-4"></div>
+    {posts.map(({ node }) => {
+      const title = node.frontmatter.title || node.fields.slug
+      return (
+        <article key={node.fields.slug} className="mt-3 mb-6">
+          <header>
+            <h3 className="inline-block font-bold hover:underline leading-tight text-lg sm:text-xl text-gray-900 dark:text-gray-200 ">
+              <TransitionLink to={node.fields.slug}>{title}</TransitionLink>
+            </h3>
+            <div className="mt-1">
+              <PostInfos
+                date={node.frontmatter.date}
+                tags={node.frontmatter.tags}
+              />
+            </div>
+          </header>
+        </article>
+      )
+    })}
+  </div>
+)
+
+const Bio = ({ photo }) => {
+  const isDarkMode = useMedia("(prefers-color-scheme: dark)")
   return (
-    <div className={marginBetweenSections}>
-      <h4 className="text-lg sm:text-xl text-gray-900 dark:text-gray-100 uppercase tracking-wide font-extrabold">
-        {title}
-      </h4>
-      <div className="sm:mt-1 h-1 gradient-right rounded w-full -p-4"></div>
-      {posts.map(({ node }) => {
-        const title = node.frontmatter.title || node.fields.slug
-        return (
-          <article key={node.fields.slug} className="mt-3 mb-6">
-            <header>
-              <h3 className="inline-block font-bold hover:underline leading-tight text-lg sm:text-xl text-gray-900 dark:text-gray-200 ">
-                <TransitionLink to={node.fields.slug}>{title}</TransitionLink>
-              </h3>
-              <div className="mt-1">
-                <PostInfos
-                  date={node.frontmatter.date}
-                  tags={node.frontmatter.tags}
-                />
-              </div>
-            </header>
-          </article>
-        )
-      })}
+    <div className="bg-gray-200 rounded p-3 sm:p-5 flex flex-col sm:flex-row items-center justify-between dark:bg-gray-800">
+      <div className="flex flex-col justify-center items-center">
+        <Img
+          fixed={photo.childImageSharp.fixed}
+          className="rounded-full shadow-lg"
+        />
+        <div className="flex space-x-4 mt-2">
+          <a
+            href="https://twitter.com/@thomas_lombart"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="p-2"
+          >
+            <img src={Twitter} alt="Twitter logo" className="h-8 w-8" />
+          </a>
+          <a
+            href="https://github.com/thomlom"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="p-2"
+          >
+            <img
+              src={isDarkMode ? GitHubLight : GitHubDark}
+              alt="GitHub logo"
+              className="h-8 w-8"
+            />
+          </a>
+        </div>
+      </div>
+      <div className="mt-4 sm:ml-8 sm:mt-0">
+        <h2 className="text-xl sm:text-2xl text-gray-800 dark:text-gray-200 font-extrabold leading-tight">
+          Hello, I'm Thomas Lombart, a french front-end engineer.{" "}
+          <span role="img" aria-label="Waving hand">
+            👋
+          </span>
+        </h2>
+        <h3 className="text-lg text-gray-700 dark:text-gray-400 font-semibold leading-snug mt-2">
+          I help developers level-up their front-end skills through high-quality
+          posts on JavaScript, modern frameworks, design, and more!
+        </h3>
+      </div>
     </div>
   )
 }
@@ -42,16 +93,14 @@ const PostSection = ({ title, posts }) => {
 const BlogIndex = ({
   data: {
     site: {
-      siteMetadata: { description, siteUrl },
+      siteMetadata: { description },
     },
-    illustration,
+    photo,
     allMdx,
   },
 }) => {
   const posts = allMdx.edges
-
   const latestPosts = posts.slice(0, 5)
-
   const popularPosts = posts.filter(
     ({
       node: {
@@ -62,15 +111,8 @@ const BlogIndex = ({
 
   return (
     <Layout>
-      <SEO
-        title="Blog"
-        description={description}
-        coverURL={siteUrl + illustration.publicURL}
-      />
-      <Img
-        fluid={illustration.childImageSharp.fluid}
-        className="rounded-lg my-0"
-      />
+      <SEO title="Blog" description={description} />
+      <Bio photo={photo} />
       <PostSection title="Latest posts 🚀" posts={latestPosts} />
       <PostSection title="Popular posts 😍" posts={popularPosts} />
       <div className={marginBetweenSections}>
@@ -90,11 +132,11 @@ export const pageQuery = graphql`
         description
       }
     }
-    illustration: file(absolutePath: { regex: "/illustration.png/" }) {
+    photo: file(absolutePath: { regex: "/photo.jpeg/" }) {
       publicURL
       childImageSharp {
-        fluid(maxHeight: 800) {
-          ...GatsbyImageSharpFluid
+        fixed(width: 125) {
+          ...GatsbyImageSharpFixed
         }
       }
     }
