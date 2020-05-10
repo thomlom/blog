c272176e1b25c5113db63194b73b68d06c0feb9a
thomlom <t.lombart97@gmail.com>
2020-05-10 10:31:22 +0200

    Remove useless usedarkmode

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 import React from "react"
 import { graphql } from "gatsby"
 import Img from "gatsby-image"
-import useDarkMode from "use-dark-mode"
 
 import Layout from "../components/layout"
 import Newsletter from "../components/newsletter"
