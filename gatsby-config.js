0b9f8aadff2a75796f4077b826a3a5f4748c8b42
thomlom <t.lombart97@gmail.com>
2020-09-10 10:04:41 +0200

    Add analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,6 +25,14 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-177646735-1",
+        anonymize: true,
+        respectDNT: true,
+      },
+    },
+    {
       resolve: `gatsby-plugin-mdx`,
       options: {
         extensions: [".mdx", ".md"],
